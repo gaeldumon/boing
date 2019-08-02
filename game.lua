e8be4a59fe18a4e641f386c989b2b678102811b0
hyperdestru <slotuser@gmail.com>
2019-08-02 21:02:05 +0200

    Refactoring process continued : pat 2. Difficulties with shots/targets join

--- game.lua
+++ game.lua
@@ -22,7 +22,7 @@ function collide(pobj1, pobj2)
 	return false
 end
 
-function create_sprite(ptype, pname, px, py, ptable)  
+function create_sprite(ptype, pname, px, py)  
     local sprite = {}
     sprite.x = px
     sprite.y = py
@@ -35,24 +35,69 @@ function create_sprite(ptype, pname, px, py, ptable)
     sprite.tframes = {}
     sprite.maxframe = 1
 
-    table.insert(ptable, sprite)
+    sprite.isoffscreen = function()
+    	if (sprite.x - sprite.w/2) > def.SCREEN_WIDTH or
+    	(sprite.x - sprite.w/2) < 0 or
+    	(sprite.y - sprite.h/2) > def.SCREEN_HEIGHT or
+    	(sprite.y - sprite.h/2) < 0 then
+    		sprite.kill = true
+    		return true
+    	else
+    		return false
+    	end 
+   	end
+
+    table.insert(this.tsprites, sprite)
 
     return sprite
 end
 
-function create_moving_obj(ptype, pname, px, py, pvx, pvy, ptable)
-    local obj = createSprite(ptype, pname, px, py)
-    obj.vx = pvx
-    obj.vy = pvy
-    table.insert(ptable, obj)
-    return obj 
+function transform_tile(pmap, pold_tile--[[tile type]], pnew_tile--[[tile id]], psound)
+    local c, l
+    for l = 1, pmap.MAP_HEIGHT do
+      	for c = 1, pmap.MAP_WIDTH do
+        	local id = pmap.grid[l][c]
+        	if pmap.tileTypes[id] == pold_tile then
+        		map.grid[l][c] = pnew_tile
+        	end
+      	end
+    end
+    psound:play()
+end
+
+function create_shot(pname, px, py, pvx, pvy)
+    local shot = createSprite('shot', pname, px, py)
+    shot.vx = pvx
+    shot.vy = pvy
+
+    shot.update = function(dt, ptargets)
+		shoot.x = shoot.x + shoot.vx * (60*dt)
+
+		local n_target
+		for n_target, target in ipairs(ptargets) do
+
+			if collide(shoot, target) == true and target.kill == false and shoot.kill == false and player.win == false then
+				player.win = true
+
+				--Star explode
+				create_explosion(target.x, target.y, this.explosion_imgs)
+
+				transform_tile(map, 'water', 4, this.sound_freeze)
+
+				shoot.kill = true
+				target.kill = true
+			end
+	end
+
+    table.insert(this.tshots, shot)
+    return shot 
 end
 
 function bounce(pobj, pgravity, psurface, pmaxy--[[150]], --[[2.5]]pforcefactor, --[[700]]pforceloss, psound, dt)
 	pobj.vy = (pgravity - pobj.force) * dt
 	pobj.y = pobj.y + pobj.vy
 
-	if pobj.y >= def.SCREEN_HEIGHT - pobj.h - pSurface then
+	if pobj.y >= psurface - pobj.h then
 		pobj.force = pgravity * pforcefactor
 		psound:play()
 	end
@@ -68,74 +113,106 @@ function create_player(px, py, pframes_table--[[this.playerImgs]])
 	player.vx = 0
 	player.force = 0
 	player.speed = 4
+	player.score = 0
+	player.win = false
+	player.lose = false
+	player.ammo = 3
 	player.tframes = pframes_table
 	player.maxframe = 4
---************************************i'm here**************************
 
 	player.moveon = function(dt)
 		player.vx = player.speed * (60*dt)
 		player.x = player.x + player.vx
+	end
 
-		if player.x - player.w/2 > def.SCREEN_WIDTH then
-			player.kill = true
-			levelUp()
-			return true
+	player.update = function(dt, ptimer)
+
+		bounce(player, this.gravity, def.SCREEN_HEIGHT - map.TILE_HEIGHT, 150, 2.5, 700, this.sound_bounce, dt)
+
+		if player.isoffscreen() == true then
+			--refresh screen
 		end
-		return false
+
+		if player.lose == true then
+			if ptimer > 0 then
+				ptimer = ptimer - (10*dt)
+			else
+				def.current_screen = 'gameover'
+			end
+		end
+
+		if player.win == true then
+			player.moveon(dt)
+			player.score = player.score + 1
+		end
 	end
 
 	return player
 end
 
-function createTarget(py, pLevel)
+function create_target(py, plevel)
 	local target = createSprite('target', 'target', def.SCREEN_WIDTH - map.TILE_WIDTH/2, py)
-	if pLevel == 1 then
+
+	if plevel == 1 then
 		target.vy = 2
-	elseif pLevel == 2 then
+	elseif plevel == 2 then
 		target.vy = 3
-	elseif pLevel >= 3 then
+	elseif plevel >= 3 then
 		target.vy = 4
 	end
+
 	target.rand_dir = math.rsign()
-	target.maxY = def.SCREEN_HEIGHT/4
-	target.minY = def.SCREEN_HEIGHT - map.TILE_HEIGHT - target.h
+	target.maxy = def.SCREEN_HEIGHT/4
+	target.miny = def.SCREEN_HEIGHT - map.TILE_HEIGHT - target.h
+
 	table.insert(this.targets, target)
 	return target
 end
 
-function createExplosion(px, py)
-	local newExplosion = createSprite('explosion', '/effects/explosion-1', px, py)
-	newExplosion.tframes = this.explosionImgs
-	newExplosion.maxFrame = 14
-	return newExplosion
+function create_explosion(px, py, pframes_table)
+	local newexplosion = createSprite('explosion', '/effects/explosion-1', px, py)
+	newexplosion.tframes = pframes_table
+	newexplosion.maxframe = 14
+	return newexplosion
 end
 
-function newGame()
-	this.current_level = 1
-	this.playerTries = 3
-	this.playerWin = false
-	this.playerLose = false
-
-	local randX = love.math.random(def.SCREEN_WIDTH/8, def.SCREEN_WIDTH/4)
-	createPlayer(def.SCREEN_WIDTH/3, 0)
-	createTarget(def.SCREEN_HEIGHT/2, this.current_level)
+function new_game()
+	local randx = love.math.random(def.SCREEN_WIDTH/8, def.SCREEN_WIDTH/4)
+	create_player(randx, 0)
+	create_target(def.SCREEN_HEIGHT/2, this.current_level)
 
 	map.load()
 end
 
-function levelUp()
-	--Re-think this : don't use newGame here, write like a "level up new game"
-	newGame()
-	this.current_level = this.current_level + 1
+function load_imgs(pdir, ptable, pnumber)
+	local i
+	for i = 1, pnumber do
+		ptable[i] = love.graphics.newImage(pdir .. tostring(i) .. '.png')
+	end
+end
+
+function kill_sprites(ptable)
+	local n
+	for n, sprite in ipairs(ptable) do
+		if sprite.kill == false then
+			sprite.kill = true
+		end
+	end
+end
+
+function purge_sprites(ptable)
+	local n
+	for n, sprite in ipairs(ptable) do
+		if sprite.kill == true then
+			table.remove(ptable, n)
+		end
+	end
 end
 
 function this.load()
-	--Re-think this : put in in levelUp ?
-	this.playerScore = 0
-
 	this.gravity = 380
 	this.PLAYER_ANIM_ON = false
-	this.timer_scoreScreen = 10
+	this.timer_scorescreen = 10
 
 	this.font = love.graphics.newFont('fonts/ArcadeAlternate.ttf', 35)
 	this.bg = love.graphics.newImage('images/bg.png')
@@ -144,112 +221,24 @@ function this.load()
 	this.sound_freeze = love.audio.newSource('sounds/freeze.wav', 'static')
 
 	this.tsprites = {}
-	this.tshoots = {}
+	this.tshots = {}
 	this.targets = {}
+	this.player_imgs = {}
+	this.explosion_imgs = {}
 
-	do
-		this.playerImgs = {}
-		local i
-		for i = 1, 4 do
-			this.playerImgs[i] = love.graphics.newImage('images/player-'..tostring(i)..'.png')
-		end
-	end
+	load_imgs('images/player-', this.player_imgs, 4)
+	load_imgs('images/effects/explosion-', this.explosion_imgs, 14)
 
-	do
-		this.explosionImgs = {}
-		local i
-		for i = 1, 14 do
-			this.explosionImgs[i] = love.graphics.newImage('images/effects/explosion-'..i..'.png')
-		end
-	end
-
-	newGame()
+	new_game()
 end
 
 function this.update(dt)
 
-	if this.playerLose == true then
-		if this.timer_scoreScreen > 0 then
-			this.timer_scoreScreen = this.timer_scoreScreen - (10*dt)
-		else
-			local n
-			for n, sprite in ipairs(this.tsprites) do
-				if sprite.kill == false then sprite.kill = true end
-			end
-			def.current_screen = 'gameover'
-		end
-	end
-
-	----MAKE PLAYER BOUNCE AND MOVE WHEN WIN
-	local shit = true
-	do
-		local n
-		for n, sprite in ipairs(this.tsprites) do
-			if sprite.type == 'player' and sprite.kill == false then
-
-				sprite.bounce(this.gravity, map.TILE_HEIGHT, 2.5, 700, this.sound_bounce, dt)
-
-				if this.playerWin == true then
-					sprite.moveOn(dt)
-				end
-			end
-		end
-	end
-	----
-
 	----UPDATE SHOOTS (MOVE, REMOVE, COLLISION WITH TARGET)
 	do
 		local n
-		for n, shoot in ipairs(this.tshoots) do
+		for n, shoot in ipairs(this.tshots) do
 
-			shoot.x = shoot.x + shoot.vx * (60*dt)
-
-			--Shoot go off screen
-			if shoot.x > def.SCREEN_WIDTH then
-				shoot.kill = true
-				table.remove(this.tshoots, n)
-
-				--LAST stone go off screen (doesn't collide with stars)
-				if this.playerTries == 0 and this.playerLose == false then
-					this.playerLose = true
-				end
-			end
-
-			----SHOOT COLLISION WITH TARGET => Water tile replaced by ice tile + freeze sound + player score update
-			local n_target
-			for n_target, target in ipairs(this.targets) do
-
-				----WIN STATE
-				if collide(shoot, target) == true and target.kill == false and shoot.kill == false and this.playerWin == false then
-					this.playerWin = true
-
-				    --Player gets a point (1 star)
-				    this.playerScore = this.playerScore + 1
-
-					--Star explode
-					createExplosion(target.x, target.y)
-
-					shoot.kill = true
-					table.remove(this.tshoots, n)
-
-					--Star disapear
-					target.kill = true
-					table.remove(this.targets, n)
-
-					----Water becomes ice
-				    local c, l
-				    for l = 1, map.MAP_HEIGHT do
-				      	for c = 1, map.MAP_WIDTH do
-				        	local id = map.grid[l][c]
-				        	if map.tileTypes[id] == 'water' then
-				        		map.grid[l][c] = 4
-				        	end
-				      	end
-				    end
-				    this.sound_freeze:play()
-				    ----
-				end
-				----
 			end
 			----
 		end
@@ -312,18 +301,11 @@ function this.update(dt)
 	end
 	----
 
-	----SPRITES PURGE
-	do
-		local n
-		for n, sprite in ipairs(this.tsprites) do
-			if sprite.kill == true then
-				table.remove(this.tsprites, n)
-			end
-		end
-	end
-	----
+	purge_sprites(this.tsprites)
 
 end
+
+
 
 function this.draw()
 
