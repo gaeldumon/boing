177797f5fe8a5bfdf60f4c109c6ba4e16405edf5
hyperdestru <slotuser@gmail.com>
2019-08-03 14:59:16 +0200

    Wrote color table and added white, black, brown, red, green, blue tables

--- define.lua
+++ define.lua
@@ -4,4 +4,12 @@ this.SCREEN_WIDTH = love.graphics.getWidth()
 this.SCREEN_HEIGHT = love.graphics.getHeight()
 this.current_screen = 'menu'
 
+this.color = {}
+this.color.white = {0,0,0}
+this.color.black = {1,1,1}
+this.color.red = {1,0,0}
+this.color.green = {0,1,0}
+this.color.blue = {0,0,1}
+this.color.brown = {102/255, 51/255, 0}
+
 return this
